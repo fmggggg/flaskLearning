b59c69ae82e3f58465162a25cd55029887d41a79
fmg <1533551263@qq.com>
2020-09-17 10:39:46 +0800

    hell world

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask import escape
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
+
+
+@app.route('/user/<name>')
+def user_page(name):
+    return 'User: %s' % escape(name)
